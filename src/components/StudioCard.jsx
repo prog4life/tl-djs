47be946716e7d3ea3512f66d316e343f491e976f
prog4life <prog4life@users.noreply.github.com>
2018-03-12 20:59:58 +0200

    Add price viewing label for StudioCard

--- src/components/StudioCard.jsx
+++ src/components/StudioCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'antd';
+// import CardExtra from 'components/CardExtra';
 
 const { Meta } = Card;
 
@@ -8,9 +9,25 @@ const StudioCard = ({ id, name, price, view: [imgSrc] }) => (
   <Card
     className="studio-card"
     cover={<img alt={`Студия ${name || ''}`} src={imgSrc} height="260" />}
+    // extra={CardExtra}
     hoverable
     // style={{ width: 382 }}
   >
+    {/* <div style={{ position: 'relative' }}> */}
+    <div style={{
+        position: 'absolute',
+        background: '#2f65eb',
+        borderRadius: '3px 0 0 3px',
+        width: '23%',
+        right: 0,
+        bottom: '80%',
+        textAlign: 'center',
+        color: 'white'
+      }}
+    >
+      {`${price} р.`}
+    </div>
+    {/* </div> */}
     <Meta
       title={`Студия ${name || ''}`}
       // description="www.whatever.com"
@@ -19,6 +36,7 @@ const StudioCard = ({ id, name, price, view: [imgSrc] }) => (
 );
 
 StudioCard.propTypes = {
+  extra: PropTypes.element.isRequired,
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
